c09a666ccc2fba6588998deef86c9e5168ff7a80
Lenovo <Lenovo@Lenovo-PC>
2021-07-10 17:41:13 -0400

    Test.java modified

--- DemoProj1/src/main/java/com/nt/test/DemoTest1.java
+++ DemoProj1/src/main/java/com/nt/test/DemoTest1.java
@@ -1,10 +1,13 @@
 package com.nt.test;
 
+import NIT.DemoProj1.App;
+
 public class DemoTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+App a=new App();
+System.out.println(a.getDate());
 	}
 
 }
